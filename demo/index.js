439b2ce22e538a1ffcf9562f1479e8b81b21a570
nitewoo <nitewoo@gmail.com>
2014-12-02 15:36:38 +0800

    fix conflict

--- demo/index.js
+++ demo/index.js
@@ -1,23 +1,8 @@
 var capital = function (input) {
       return input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-angular.module('ui.bootstrap.demo', []);
-angular.module('app', ['ui.bootstrap', 'ui.router', 'ui.bootstrap.demo', 'ui.yt', 'modalBuild'])
-=======
 angular.module('ui.yt.demo', []);
-angular.module('app', ['ui.router', 'ui.yt.demo', 'ui.yt', 'ui.bootstrap'])
->>>>>>> FETCH_HEAD
-=======
-angular.module('ui.yt.demo', []);
-angular.module('app', ['ui.router', 'ui.yt.demo', 'ui.yt', 'ui.bootstrap'])
->>>>>>> 8e25d866859f79ead576b4421b0c159b7d4f75b7
-=======
-angular.module('ui.yt.demo', []);
-angular.module('app', ['ui.router', 'ui.yt.demo', 'ui.yt', 'ui.bootstrap'])
->>>>>>> 8e25d866859f79ead576b4421b0c159b7d4f75b7
+angular.module('app', ['ui.router', 'ui.yt.demo', 'ui.yt', 'ui.bootstrap', 'modalBuild'])
   .constant('COMPONENTS', [
     {
       name: 'alert'
--- demo/vendor/ui-yt.js
+++ demo/vendor/ui-yt.js
@@ -830,25 +830,6 @@ module.run(["$templateCache", function($templateCache) {
 })();
 
 (function(module) {
-try { module = angular.module("dropdownlist/template/dropdown.html"); }
-catch(err) { module = angular.module("dropdownlist/template/dropdown.html", []); }
-module.run(["$templateCache", function($templateCache) {
-  $templateCache.put("dropdownlist/template/dropdown.html",
-    "\n" +
-    "\n" +
-    "<div class=\"dropdown btn-group w_100p\">\n" +
-    "  <button class=\"dropdown-toggle btn btn-default btn-full-width\" ng-disabled=\"dropdownDisabled\">\n" +
-    "    <span class=\"col-md-11 dropdown-text\">{{curText}}</span>\n" +
-    "    <span class=\"caret\"></span>\n" +
-    "  </button>\n" +
-    "  <ul class=\"dropdown-menu\">\n" +
-    "  </ul>\n" +
-    "</div>\n" +
-    "");
-}]);
-})();
-
-(function(module) {
 try { module = angular.module("confirm/template/wrapper.html"); }
 catch(err) { module = angular.module("confirm/template/wrapper.html", []); }
 module.run(["$templateCache", function($templateCache) {
@@ -870,6 +851,25 @@ module.run(["$templateCache", function($templateCache) {
     "    </div>\n" +
     "  </div>\n" +
     "</div>");
+}]);
+})();
+
+(function(module) {
+try { module = angular.module("dropdownlist/template/dropdown.html"); }
+catch(err) { module = angular.module("dropdownlist/template/dropdown.html", []); }
+module.run(["$templateCache", function($templateCache) {
+  $templateCache.put("dropdownlist/template/dropdown.html",
+    "\n" +
+    "\n" +
+    "<div class=\"dropdown btn-group w_100p\">\n" +
+    "  <button class=\"dropdown-toggle btn btn-default btn-full-width\" ng-disabled=\"dropdownDisabled\">\n" +
+    "    <span class=\"col-md-11 dropdown-text\">{{curText}}</span>\n" +
+    "    <span class=\"caret\"></span>\n" +
+    "  </button>\n" +
+    "  <ul class=\"dropdown-menu\">\n" +
+    "  </ul>\n" +
+    "</div>\n" +
+    "");
 }]);
 })();
 
